05e432a9f903812024f63e9304681261d63fea7c
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2024-08-13 06:23:23 -0700

    feat: add cmake snippets

--- after/plugin/luasnippets/snippets.lua
+++ after/plugin/luasnippets/snippets.lua
@@ -12,7 +12,7 @@ ls.add_snippets("all", {
         i(1, "cond"), t(" ? "), i(2, "then"), t(" : "), i(3, "else")
     }),
     s("t", {
-        c(1, {t("choice 1"), t("choice 2")})
+        c(1, { t("choice 1"), t("choice 2") })
     })
 })
 
@@ -22,7 +22,13 @@ ls.add_snippets("lua", {
     }),
     s("req", fmt("local {} = require('{}')", { i(1, "default"), rep(1) })),
     s("s", {
-        t("s(\""), i(1), t({"\", {", "    "}), i(2), t({"", "}),"})
+        t("s(\""), i(1), t({ "\", {", "    " }), i(2), t({ "", "})," })
     }),
 })
 
+ls.add_snippets("cmake", {
+    s("cc", {
+        t({ [[set(CMAKE_EXPORT_COMPILE_COMMANDS ON)]], [[file(CREATE_LINK ${CMAKE_BINARY_DIR}/compile_commands.json]],
+            [[     ${CMAKE_SOURCE_DIR}/compile_commands.json SYMBOLIC)]] }),
+    }),
+})
