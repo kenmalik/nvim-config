59b4c3ff6f285842a083115e3e0a3eb97b40ee8c
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2024-08-13 06:15:49 -0700

    feat: set default tabwidth to 2

--- lua/kenmalik/set.lua
+++ lua/kenmalik/set.lua
@@ -1,9 +1,9 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
@@ -32,3 +32,4 @@ vim.g.maplocalleader = " "
 
 vim.opt.guicursor = "n-v-c:block-blinkon1000-blinkoff1000"
 
+vim.opt.clipboard:append { 'unnamed', 'unnamedplus' }
