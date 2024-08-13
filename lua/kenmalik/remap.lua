9f0925855932298345aa2ffc85b6bc818bb0d272
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2024-08-13 06:46:05 -0700

    feat: add tmux sessionizer remap

--- lua/kenmalik/remap.lua
+++ lua/kenmalik/remap.lua
@@ -23,3 +23,4 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set("i", "<C-c>", "<Esc>")
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
