ec47765b57c8e1805716ebae604f94c536deb7aa
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2025-01-03 21:34:00 -0800

    Add mini icons

--- lua/config/plugins/mini.lua
+++ lua/config/plugins/mini.lua
@@ -4,6 +4,8 @@ return {
 		config = function()
 			local statusline = require "mini.statusline"
 			statusline.setup { use_icons = true }
+      local icons = require "mini.icons"
+      icons.setup {}
 		end
 	}
 }
