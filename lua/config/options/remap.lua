e3d10e8ad23aba2f1c3d5b0a38134bed530ac1f3
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2025-01-12 23:46:47 -0800

    Remap <leader>s with <leader>r
    Remapped to remove conflicts with mini.surround mappings

--- lua/config/options/remap.lua
+++ lua/config/options/remap.lua
@@ -18,7 +18,7 @@ vim.keymap.set("n", "<leader>Y", "\"+Y")
 vim.keymap.set("n", "<leader>d", "\"_d")
 vim.keymap.set("v", "<leader>d", "\"_d")
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set("i", "<C-c>", "<Esc>")
