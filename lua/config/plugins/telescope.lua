39398423da40d1e1b941b6010bbc78850a2f9f12
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2025-09-01 23:20:15 -0700

    Add document symbol search to Telescope

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -7,6 +7,7 @@ return {
     vim.keymap.set('n', '<leader>pf', builtin.find_files)
     vim.keymap.set('n', '<leader>pg', builtin.git_files)
     vim.keymap.set('n', '<leader>ps', builtin.live_grep)
+    vim.keymap.set('n', '<leader>gO', builtin.lsp_document_symbols)
     vim.keymap.set('n', '<leader>fh', builtin.help_tags)
     vim.keymap.set('n', '<leader>fw', builtin.grep_string)
   end
