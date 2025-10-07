e0e9c2d891a72502599d824aa7fcd36f34fdf241
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2025-10-07 16:51:24 -0700

    Add dap

--- lua/config/options/init.lua
+++ lua/config/options/init.lua
@@ -1,3 +1,4 @@
 require("config.options.remap")
 require("config.options.set")
 require("config.options.lsp")
+require("config.options.dap")
--- lua/config/options/lsp.lua
+++ lua/config/options/lsp.lua
@@ -1,9 +1,13 @@
-require('lspconfig').clangd.setup{
-  cmd = {
-    "clangd",
-    "--query-driver=/usr/bin/c++,/usr/bin/g++,/usr/bin/clang++",
-    "--compile-commands-dir=build",
-  },
-}
+vim.lsp.config("clangd", {
+	cmd = {
+		"clangd",
+		"--query-driver=/usr/bin/c++,/usr/bin/g++,/usr/bin/clang++",
+		"--compile-commands-dir=build",
+	},
+})
+
+vim.lsp.config("pyright", {})
 
 vim.lsp.enable("clangd")
+vim.lsp.enable("pyright")
+vim.lsp.enable("typescript-language-server")
