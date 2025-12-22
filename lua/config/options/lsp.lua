c6b1912d0263c2aec3dfa0f640928d145bc494ce
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2025-12-22 15:46:24 -0800

    Add mason-lspconfig

--- lua/config/options/lsp.lua
+++ lua/config/options/lsp.lua
@@ -5,28 +5,13 @@ vim.lsp.config("clangd", {
 		"--compile-commands-dir=build",
 	},
 })
-vim.lsp.enable("clangd")
-
-vim.lsp.config("pyright", {})
-vim.lsp.enable("pyright")
 
 vim.lsp.config("typescript-language-server", {
 	cmd = { "typescript-language-server", "--stdio" },
 	filetypes = { "typescript", "typescriptreact" },
 })
-vim.lsp.enable("typescript-language-server")
-
-vim.lsp.config("emmet_language_server", {})
-vim.lsp.enable("emmet_language_server")
-
-vim.lsp.config("jdtls", {})
-vim.lsp.enable("jdtls")
 
 vim.lsp.config("cmake-language-server", {
 	command = { "cmake-language-server" },
 	filetypes = { "cmake" },
 })
-vim.lsp.enable("cmake-language-server")
-
-vim.lsp.config("lua_ls", {})
-vim.lsp.enable("lua_ls")
--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -1,50 +1,57 @@
 return {
-    { "mason-org/mason.nvim", opts = {} },
-    {
-        "neovim/nvim-lspconfig",
-        dependencies = {
-            {
-                "folke/lazydev.nvim",
-                ft = "lua", -- only load on lua files
-                opts = {
-                    library = {
-                        -- See the configuration section for more details
-                        -- Load luvit types when the `vim.uv` word is found
-                        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-                    },
-                },
-            },
-        },
-        config = function()
-            vim.api.nvim_create_autocmd("LspAttach", {
-                callback = function()
-                    vim.keymap.set("n", "gd", function()
-                        vim.lsp.buf.definition()
-                    end)
-                    vim.keymap.set("n", "<leader>vws", function()
-                        vim.lsp.buf.workspace_symbol()
-                    end)
-                    vim.keymap.set("n", "<leader>vd", function()
-                        vim.diagnostic.open_float()
-                    end)
-                    vim.keymap.set("n", "<leader>vca", function()
-                        vim.lsp.buf.code_action()
-                    end)
-                    vim.keymap.set("n", "<leader>vrr", function()
-                        vim.lsp.buf.references()
-                    end)
-                    vim.keymap.set("n", "<leader>vrn", function()
-                        vim.lsp.buf.rename()
-                    end)
-                    vim.keymap.set("i", "<C-h>", function()
-                        vim.lsp.buf.signature_help()
-                    end)
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = {},
+		dependencies = {
+			{ "mason-org/mason.nvim", opts = {} },
+			"neovim/nvim-lspconfig",
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			{
+				"folke/lazydev.nvim",
+				ft = "lua", -- only load on lua files
+				opts = {
+					library = {
+						-- See the configuration section for more details
+						-- Load luvit types when the `vim.uv` word is found
+						{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+					},
+				},
+			},
+		},
+		config = function()
+			vim.api.nvim_create_autocmd("LspAttach", {
+				callback = function()
+					vim.keymap.set("n", "gd", function()
+						vim.lsp.buf.definition()
+					end)
+					vim.keymap.set("n", "<leader>vws", function()
+						vim.lsp.buf.workspace_symbol()
+					end)
+					vim.keymap.set("n", "<leader>vd", function()
+						vim.diagnostic.open_float()
+					end)
+					vim.keymap.set("n", "<leader>vca", function()
+						vim.lsp.buf.code_action()
+					end)
+					vim.keymap.set("n", "<leader>vrr", function()
+						vim.lsp.buf.references()
+					end)
+					vim.keymap.set("n", "<leader>vrn", function()
+						vim.lsp.buf.rename()
+					end)
+					vim.keymap.set("i", "<C-h>", function()
+						vim.lsp.buf.signature_help()
+					end)
 
-                    vim.keymap.set({ "n", "x" }, "gq", function()
-                        vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
-                    end)
-                end,
-            })
-        end,
-    },
+					vim.keymap.set({ "n", "x" }, "gq", function()
+						vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
+					end)
+				end,
+			})
+		end,
+	},
 }
