7ab3bd8849d18480602041a7b1785956e652aff8
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2026-05-08 10:34:06 -0700

    Update Telescope config

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -1,15 +1,16 @@
 return {
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.8",
-    -- or                              , branch = '0.1.x',
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-        local builtin = require("telescope.builtin")
-        vim.keymap.set("n", "<leader>pf", builtin.find_files)
-        vim.keymap.set("n", "<leader>pg", builtin.git_files)
-        vim.keymap.set("n", "<leader>ps", builtin.live_grep)
-        vim.keymap.set("n", "<leader>gO", builtin.lsp_document_symbols)
-        vim.keymap.set("n", "<leader>fh", builtin.help_tags)
-        vim.keymap.set("n", "<leader>fw", builtin.grep_string)
-    end,
+    version = "*",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    },
+    keys = {
+        { "<leader>pf", require("telescope.builtin").find_files,           desc = "Find files" },
+        { "<leader>pg", require("telescope.builtin").git_files,            desc = "Find git files" },
+        { "<leader>ps", require("telescope.builtin").live_grep,            desc = "Live grep" },
+        { "<leader>gO", require("telescope.builtin").lsp_document_symbols, desc = "Find LSP symbols" },
+        { "<leader>fh", require("telescope.builtin").help_tags,            desc = "Find help tags" },
+        { "<leader>fw", require("telescope.builtin").grep_string,          desc = "Grep string under cursor" },
+    },
 }
