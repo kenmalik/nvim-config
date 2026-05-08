9d3ae9c182675ca693f584dc70183e43f9225bde
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2026-05-08 11:59:23 -0700

    Add completion

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -4,20 +4,19 @@ return {
         opts = {},
         dependencies = {
             { "mason-org/mason.nvim", opts = {} },
-            "neovim/nvim-lspconfig",
-        },
-    },
-    {
-        "neovim/nvim-lspconfig",
-        dependencies = {
             {
-                "folke/lazydev.nvim",
-                ft = "lua", -- only load on lua files
-                opts = {
-                    library = {
-                        -- See the configuration section for more details
-                        -- Load luvit types when the `vim.uv` word is found
-                        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                "neovim/nvim-lspconfig",
+                dependencies = {
+                    {
+                        "folke/lazydev.nvim",
+                        ft = "lua", -- only load on lua files
+                        opts = {
+                            library = {
+                                -- See the configuration section for more details
+                                -- Load luvit types when the `vim.uv` word is found
+                                { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                            },
+                        },
                     },
                 },
             },
